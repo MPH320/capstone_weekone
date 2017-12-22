b1a64297ef4c89a1a4b426d6f317af90c057d1e7
Michelangelo <michelangelo.ph@gmail.com>
2017-12-21 20:18:03 -0800

    City and state resources added

--- spec/requests/api_developments_spec.rb
+++ spec/requests/api_developments_spec.rb
@@ -8,35 +8,35 @@ RSpec.describe "ApiDevelopments", type: :request do
 	end
 
 	describe "RDBMS-backed" do
-		before(:each) { Cities.delete_all }
-		after(:each) { Cities.delete_all }
+		before(:each) { City.delete_all }
+		after(:each) { City.delete_all }
 		
 		it "create RDBMS-backed model" do 
-			object=Cities.create(:name=>"test")
-			expect(Cities.find(object.id).name).to eq("test")
+			object=City.create(:name=>"test")
+			expect(City.find(object.id).name).to eq("test")
 		end
 		
 		it "expose RDBMS-backed API resource" do
-			object=Cities.create(:name=>"test")
+			object=City.create(:name=>"test")
 			expect(cities_path).to eq("/api/cities")
-			get cities_path(object.id)
+			get city_path(object.id)
 			expect(response).to have_http_status(:ok)
 			expect(parsed_body["name"]).to eq("test")
 		end
 	end
 	
 	describe "MONGODB-backed" do
-		before(:each) { States.delete_all }
-		after(:each) { States.delete_all }
+		before(:each) { State.delete_all }
+		after(:each) { State.delete_all }
 		
 		it "create MONGODB-backed model" do
-			object=States.create(:name=>"test")
-			expect(States.find(object.id).name).to eq("test")
+			object=State.create(:name=>"test")
+			expect(State.find(object.id).name).to eq("test")
 		end
 		it "expose MONGODB-backed API resource" do
-			object=States.create(:name=>"test")
+			object=State.create(:name=>"test")
 			expect(states_path).to eq("/api/states")
-			get states_path(object.id)
+			get state_path(object.id)
 			expect(response).to have_http_status(:ok)
 			expect(parsed_body["name"]).to eq("test")
 			expect(parsed_body).to include("created_at")
