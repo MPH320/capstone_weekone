10da7aab908663948df8e521fbff27bea3656892
Michelangelo <michelangelo.ph@gmail.com>
2018-01-11 11:08:45 -0800

    Cities controller skeleton added

---
+++ app/assets/javascripts/spa-demo/cities/cities.controller.js
@@ -0,0 +1,46 @@
+(function() {
+  "use strict";
+
+  angular
+    .module("spa-demo.cities")
+    .controller("spa-demo.cities.CitiesController", CitiesController);
+
+  CitiesController.$inject = ["spa-demo.cities.City"];
+
+  function CitiesController(City) {
+      var vm = this;
+      vm.cities;
+			vm.city;
+
+      activate();
+ 
+      function activate() {
+   			newCity();
+      }
+		
+		  function newCity() {
+        vm.city = new City();
+      }
+      function handleError(response) {
+        console.log(response);
+      } 
+      function edit(object) {
+       
+      }
+      function create() {
+      
+      }
+      function update() {
+      
+      }
+      function remove() {
+       
+      }
+
+      function removeElement(elements, element) {
+       
+      }  
+  
+
+  }
+})();
