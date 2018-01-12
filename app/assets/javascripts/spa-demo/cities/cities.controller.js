bc039f663d62263850964f870b30cfac60153d6a
Michelangelo <michelangelo.ph@gmail.com>
2018-01-12 12:13:10 -0800

    Can select, update, and delete resources

--- app/assets/javascripts/spa-demo/cities/cities.controller.js
+++ app/assets/javascripts/spa-demo/cities/cities.controller.js
@@ -11,8 +11,14 @@
       var vm = this;
       vm.cities;
 			vm.city;
-
+			vm.edit = edit;
+      vm.create = create;
+      vm.update = update;
+      vm.remove = remove;
+		
       activate();
+		
+			return;
  
       function activate() {
    			newCity();
@@ -26,20 +32,43 @@
         console.log(response);
       } 
       function edit(object) {
-       
+        console.log("selected", object);
+        vm.city = object;
       }
-      function create() {
-      
+ 			function create() {
+			
+        vm.city.$save()
+        .then(function(response){
+            //console.log(response);
+            vm.cities.push(vm.city);
+            newCity();
+        })
+        .catch(handleError);
       }
       function update() {
-      
+        vm.city.$update()
+        .then(function(response){
+            //console.log(response);
+        })
+        .catch(handleError);
       }
       function remove() {
-       
+        vm.city.$delete()
+        .then(function(response){
+            //console.log(response);
+            removeElement(vm.cities, vm.city);
+            newCity();
+        })
+        .catch(handleError);
       }
 
       function removeElement(elements, element) {
-       
+        for (var i=0; i<elements.length; i++) {
+          if (elements[i].id == element.id) {
+            elements.splice(i,1);
+            break;
+          }        
+        } 
       }  
   
 
