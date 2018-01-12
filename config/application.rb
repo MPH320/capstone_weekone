0fef2f8955c8f2a2fc57083e3cb1fd66d8ab3214
Michelangelo <michelangelo.ph@gmail.com>
2018-01-12 13:07:51 -0800

    added cors heroku origin

--- config/application.rb
+++ config/application.rb
@@ -30,7 +30,7 @@ module CapstoneWeekone
 		
 		config.middleware.insert_before 0, "Rack::Cors" do
       allow do
-        origins 'https://photo-staging-week-two.herokuapp.com'
+        origins /https:\/\/\w+\.github.io/
 
         resource 'api/*', 
           :headers => :any, 
